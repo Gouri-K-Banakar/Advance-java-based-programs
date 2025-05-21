fb953161fdbb94cb0b08217a9a35b442c9835154
Gouri-K-Banakar <banakargouri4@gmail.com>
2025-05-21 23:41:34 +0530

    Update TabbedPan.java

--- 5_SwingPrograms/TabbedPan.java
+++ 5_SwingPrograms/TabbedPan.java
@@ -1,4 +1,4 @@
-Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and
+//Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and
 display the concerned color whenever the specific tab is selected in the Pan.
 package javaass;
 import javax.swing.*;
