2b9b40bb5456c5ce7674d7207325fcda33c00e77
Gouri-K-Banakar <banakargouri4@gmail.com>
2025-05-22 00:02:40 +0530

    Update HelloSwing.java

--- 4_SwingPrograms/HelloSwing.java
+++ 4_SwingPrograms/HelloSwing.java
@@ -1,5 +1,4 @@
-// Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing
-Programming in Blue color plain font with font size of 32 using Jframe and Jlabelpackage javaass;
+// Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing Programming in Blue color plain font with font size of 32 using Jframe and Jlabelpackage javaass;
 
 
 import javax.swing.*;
