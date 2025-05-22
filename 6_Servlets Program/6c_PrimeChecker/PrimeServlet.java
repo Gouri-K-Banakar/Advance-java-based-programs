1a5988fea643311f719f5283c427c0f4369edce2
Gouri-K-Banakar <banakargouri4@gmail.com>
2025-05-22 12:37:14 +0530

    Update PrimeServlet.java

--- 6_Servlets Program/6c_PrimeChecker/PrimeServlet.java
+++ 6_Servlets Program/6c_PrimeChecker/PrimeServlet.java
@@ -1,5 +1,6 @@
-// Build a servlet program to check the given number is prime number or not using HTML with step
-by step procedure.
+*/ Build a servlet program to check the given number is prime number or not using HTML with step
+by step procedure.*/
+    
 package com.prime;
 
 import java.io.IOException;
